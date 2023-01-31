fc7273c7af9310315a66c5c71c04bb4a10bc9660
Valerie Martin <78977660+ValM11@users.noreply.github.com>
2023-01-31 17:11:55 +0100

    Implement weighted score

--- src/PerformanceCriterion.ts
+++ src/PerformanceCriterion.ts
@@ -1,33 +1,37 @@
 export interface Criterion {
-  id: number
-  name: string
-  description: string
-  value: number
+  id: number;
+  name: string;
+  description: string;
+  value: number;
+  weight: number;
 }
 
 export interface CriterionExtended {
-  getCriterionName(): string
+  getCriterionName(): string;
 }
 
 export class PerformanceCriterion implements Criterion, CriterionExtended {
-  id: number
-  name: string
-  description: string
-  value: number
+  id: number;
+  name: string;
+  description: string;
+  value: number;
+  weight: number;
 
   constructor(
     c_id: number,
     c_name: string,
     c_description: string,
-    c_value: number
+    c_value: number,
+    c_weight: number
   ) {
-    this.id = c_id
-    this.name = c_name
-    this.description = c_description
-    this.value = c_value
+    this.id = c_id;
+    this.name = c_name;
+    this.description = c_description;
+    this.value = c_value;
+    this.weight = c_weight;
   }
 
   getCriterionName(): string {
-    return this.name
+    return this.name;
   }
 }
